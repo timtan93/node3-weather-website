92267d0adff7ac8475be0ee3419845f01e526b67
timtan93 <timtan93@gmail.com>
2019-04-05 11:27:05 +0100

    added high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -17,9 +17,9 @@ const forecast = (latitude, longitude, callback) => {
       callback(
         undefined,
         body.daily.data[0].summary +
-          "It is currently " +
+          " It is currently " +
           +temp +
-          " degrees out. There is a " +
+          " degrees out. The high today is " + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow +  ". There is a " +
           rain +
           "% chance of rain."
         
